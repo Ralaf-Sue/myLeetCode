1b8ce6155a8fb79e27b6ef1a7c9525d597a19c1b
suzq <suzq@broada.com>
2021-03-23 18:07:28 +0800

    feat:希尔排序

--- src/sort/README.md
+++ src/sort/README.md
@@ -1,3 +1,4 @@
 [冒泡排序原理](https://pic1.zhimg.com/v2-1543c0b97237bb55063e033959706ca0_b.webp)  
 [插入排序原理](https://pic2.zhimg.com/v2-f87ad7d8ad54379dd81f02fcf9b91f49_b.jpg)  
-[选择排序原理](https://pic1.zhimg.com/v2-f20b8898585b3ca03843d93ce2c35a68_b.webp)
+[选择排序原理](https://pic1.zhimg.com/v2-f20b8898585b3ca03843d93ce2c35a68_b.webp)  
+希尔排序：对数据进行逻辑分组，然后组内进行排序，当组的步长是1的时候，排序完成  
--- src/sort/SortUtil.java
+++ src/sort/SortUtil.java
@@ -12,11 +12,12 @@ import java.util.Objects;
  */
 public class SortUtil {
 
-    public static void main(String[] args){
-        int[] arr = {12,3,33,3,55,1,29,24} ;
+    public static void main(String[] args) {
+        int[] arr = {12, 3, 33, 3, 55, 24, 79, 03, 42, 29, 24};
 //        bubbleSort(arr);
 //        insertSort(arr);
-        chooseSort(arr);
+//        chooseSort(arr);
+        shellSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -43,17 +44,17 @@ public class SortUtil {
         if (Objects.isNull(arr) || arr.length < 2) return;
         int size = arr.length;
         //假设前面的数字都是正确的顺序
-        for (int i=0;i<size-1;i++){
-            for(int j =i+1;j>0 ;j--){
-                int temp = arr[j] ;
-                if(arr[j]<arr[j-1]){
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                int temp = arr[j];
+                if (arr[j] < arr[j - 1]) {
                     //将前面数往后挪，空出位置，后数插入
-                    int k = arr[j-1] ;
-                    arr[j] = k ;
-                    arr[j-1] = arr[j] ;
+                    int k = arr[j - 1];
+                    arr[j] = k;
+                    arr[j - 1] = arr[j];
 
-                }else{
-                    break ;
+                } else {
+                    break;
                 }
             }
 
@@ -66,21 +67,50 @@ public class SortUtil {
     //选择排序，总是在选择后面的数中最小的，跟要排序的的数进行交换
     public static void chooseSort(int[] arr) {
         if (Objects.isNull(arr) || arr.length < 2) return;
-        int size = arr.length ;
-        for(int i = 0;i<size-1;i++){
+        int size = arr.length;
+        for (int i = 0; i < size - 1; i++) {
 
-            int minIndex = i ;
-            for(int j = i+1 ;j<size;j++){
-                if(arr[minIndex]>arr[j])
-                    minIndex = j ;
+            int minIndex = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[minIndex] > arr[j])
+                    minIndex = j;
             }
-            if(minIndex!=i){
+            if (minIndex != i) {
                 int temp = arr[i];
-                arr[i] =arr[minIndex] ;
-                arr[minIndex] = temp ;
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
             }
         }
 
     }
+
+    //希尔排序
+    //插入排序的变种，先逻辑分组，分组内进行调整
+    public static void shellSort(int[] arr) {
+        if (Objects.isNull(arr) || arr.length < 2) return;
+        int size = arr.length;
+        //确定每次的偏移量
+        for (int i = size / 2; i > 0; i /= 2) {
+            //对每个分组进行插入排序
+            //获取每组的起始索引
+            System.out.println(arr);
+            for (int j = 0; j < i; j++) {
+                //组内排序
+                for (int h = j; h < size - i; h += i) {
+                    for (int k = h + i; k > h; k -= i) {
+                        if (arr[k-i] > arr[k]) {
+                            int temp = arr[k];
+                            arr[k] = arr[k-i];
+                            arr[k-i] = temp;
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+    }
+
 
 }
